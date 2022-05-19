1c8329b958e23ba5c140dd18522559dfb3811293
rdaniliuk <r.daniliuk.22@gmail.com>
2022-05-20 00:33:34 +0200

    feat: complete copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 const path = require('path');
+const folder = path.join(__dirname, 'files');
+const folderCopy = path.join(__dirname, 'files-copy');
 
 
-fs.mkdir(path.join(__dirname, 'files-copy'), err => { 
-   if (err) throw err;
+fs.rm(folderCopy, {recursive: true}, () => {
+  fs.mkdir(folderCopy, () => {
+    fs.readdir(folder, (err, files) => {
+      files.forEach(fileName => {
+        let filePath = path.join(folder, fileName);
+        let filePathCopy = path.join(folderCopy, fileName);
+        fs.copyFile(filePath, filePathCopy, () => {});
+      });
+    });
+  });
 });
