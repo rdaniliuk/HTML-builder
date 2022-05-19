29f466a19dfb0b9b25e8b3b97230b4f363e4b01d
rdaniliuk <r.daniliuk.22@gmail.com>
2022-05-19 23:27:54 +0200

    feat: complete files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+
+fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
+  if (err) { throw err;}
+  files.forEach((fileName) => {
+    let {ext, name} = path.parse(fileName);
+    fs.stat(path.join(__dirname, 'secret-folder', fileName), (err, stats) => {
+      if (err) { throw err;}
+      let size = stats.size / 1000;
+      console.log(`${name} - ${ext.slice(1)} - ${size}kb`);
+    });
+  });
+});
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,7 @@
+const fs = require('fs');
+const path = require('path');
 
+
+fs.mkdir(path.join(__dirname, 'files-copy'), err => { 
+   if (err) throw err;
+});
